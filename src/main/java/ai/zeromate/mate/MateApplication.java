c67c37d00e9792b0c5e3a3c83a259236676dc245
Vova Prudnikov <vovkasmail@gmail.com>
2025-03-19 16:18:46 -0700

    ZERO-0: project setup

---
+++ src/main/java/ai/zeromate/mate/MateApplication.java
@@ -0,0 +1,13 @@
+package ai.zeromate.mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/zeromate/mate/MateApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.zeromate.mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
